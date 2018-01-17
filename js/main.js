cb836625a10035007b17444efa872478bdd70601
Juan Diego Araya <juand.95820@gmail.com>
2018-01-17 09:27:35 -0600

    Webpage CSS JS HTML only

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// function animationHover(element, animation){
+//     element = $(element);
+//     element.hover(
+//         function() {
+//             element.addClass('animated ' + animation);        
+//         },
+//         function(){
+//             //wait for animation to finish before removing classes
+//             window.setTimeout( function(){
+//                 element.removeClass('animated ' + animation);
+//             }, 2000);         
+//         });
+// }
+
+$(function() {
+    $.scrollify({
+      section : ".sec",
+      scrollbars : true,
+      scrollSpeed : 1500 
+    });
+  });
+
+  function move(section){
+    $.scrollify.move(section);
+  }
